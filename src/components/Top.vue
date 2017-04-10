3d4c7de996be8e90663bf1360e39090d6b7a774c
shengyang <123456>
2017-04-10 15:41:19 +0800

    首页优化

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -4,14 +4,14 @@
       <div class="container">
         <div class="nav-left">
           <a class="nav-item" href="home.html">
-            xxweimei blog
+            xxweimei
           </a>
         </div>
         <div class="nav-right nav-menu">
           <a class="nav-item" v-bind:class="{'is-active':index===0}" href="home.html">Home</a>
           <a class="nav-item" v-bind:class="{'is-active':index===1}" href="">Blog</a>
           <a class="nav-item" v-bind:class="{'is-active':index===2}" href="game.html">Game</a>
-          <a class="nav-item" v-bind:class="{'is-active':index===3}" href="">Github</a>
+          <a class="nav-item" v-bind:class="{'is-active':index===3}" href="https://github.com/xxweimei" target="_blank">Github</a>
           <a class="nav-item" v-bind:class="{'is-active':index===4}" href="">About</a>
           <a class="nav-item" v-bind:class="{'is-active':index===5}" href="">Donate</a>
         </div>
