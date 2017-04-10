46e9540bc89b6ffd3f0ce86252ce6fc3b760cd60
shengyang <123456>
2017-04-10 16:18:24 +0800

    游戏模块修改

--- src/module/game/Game.vue
+++ src/module/game/Game.vue
@@ -5,113 +5,27 @@
         <Top :index="2"></Top>
       </div>
     </section>
-    <div class="section">
-      <div class="">
-        <div class="boards">
-          <div class="columns">
-            <div class="column is-3 board" id="b1">
-              <div class="box is-gray">
-                <div class="head">
-                  <div class="name">Backlog</div>
-                  <div class="count">1</div>
-                  <div class="options"><i class="fa fa-ellipsis-h"></i></div>
-                </div>
-                <div class="items">
-                  <div class="box" id="b1c1">
-                    <p class="title">test item</p>
-                    <p class="meta">
-                      <img src="https://placehold.it/28x28">
-                      <a href="#"><i class="fa fa-bars"></i></a>
-                    </p>
-                  </div>
-                  <div class="add-card">
-                    <div onclick="addCard(this)">
-                      <i class="fa fa-plus-circle"></i>
-                      Add new card
-
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <div class="container section">
+      <div class="columns">
+        <div class="column is-4">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                三消小游戏-宠物小精灵版
+              </p>
+            </header>
+            <div class="card-image">
+              <figure class="image is-square">
+                <img src="../../assets/img/eliminate.png" alt="Image">
+              </figure>
             </div>
-            <div class="column is-3 board" id="b2">
-              <div class="box is-gray">
-                <div class="head">
-                  <div class="name">Upcoming</div>
-                  <div class="count">1</div>
-                  <div class="options"><i class="fa fa-ellipsis-h"></i></div>
-                </div>
-                <div class="items">
-                  <div class="box" id="b2c1">
-                    <p class="title">Update middle-out compression algorithm</p>
-                    <p class="meta">
-                      <img src="https://placehold.it/28x28">
-                      <a href="#"><i class="fa fa-bars"></i></a>
-                    </p>
-                  </div>
-                  <div class="add-card">
-                    <div onclick="addCard(this)">
-                      <i class="fa fa-plus-circle"></i>
-                      Add new card
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column is-3 board" id="b3">
-              <div class="box is-gray">
-                <div class="head">
-                  <div class="name">In Progress</div>
-                  <div class="count">1</div>
-                  <div class="options"><i class="fa fa-ellipsis-h"></i></div>
-                </div>
-                <div class="items">
-                  <div class="box" id="b3c1">
-                    <p class="title">Send project notification to users</p>
-                    <p class="meta">
-                      <img src="https://placehold.it/28x28">
-                      <a href="#"><i class="fa fa-bars"></i></a>
-                    </p>
-                  </div>
-                  <div class="add-card">
-                    <div onclick="addCard(this)">
-                      <i class="fa fa-plus-circle"></i>
-                      Add new card
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column is-3 board" id="b4">
-              <div class="box is-gray">
-                <div class="head">
-                  <div class="name">Complete</div>
-                  <div class="count">1</div>
-                  <div class="options"><i class="fa fa-ellipsis-h"></i></div>
-                </div>
-                <div class="items">
-                  <div class="box" id="b4c1">
-                    <p class="title">Create first test</p>
-                    <p class="meta">
-                      <img src="https://placehold.it/28x28">
-                      <a href="#"><i class="fa fa-bars"></i></a>
-                      <a href="#"><i class="fa fa-comment-o"></i></a>
-                    </p>
-                  </div>
-                  <div class="add-card">
-                    <div onclick="addCard(this)">
-                      <i class="fa fa-plus-circle"></i>
-                      Add new card
-
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div class="card-content has-text-right">
+              <p><a class="button is-info">Play</a></p>
             </div>
           </div>
         </div>
+        <div class="column is-4"></div>
+        <div class="column is-4"></div>
       </div>
     </div>
     <Bottom></Bottom>
