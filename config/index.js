71094dddcc6dcb4de94a2e7ca75c9593bd6f19e7
xxweimei <xxweimei@gmail.com>
2017-08-10 09:23:07 +0800

    打包修改

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'blog',
     assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
